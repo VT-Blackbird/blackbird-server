d94a7c67d754c110cce6272c545f510ec27501fb
hcallie21 <149312971+hcallie21@users.noreply.github.com>
2026-04-23 23:46:01 -0400

    Feat/grounds and outliers (#39)
    * File Renaming
    
    * thresholding
    
    * typo in Readme

--- app/workers/scrapers/gov_scraper.py
+++ app/workers/scrapers/gov_scraper.py
@@ -5,9 +5,9 @@ from typing import Any, Dict, List, Optional, Tuple
 from bs4 import BeautifulSoup
 
 from app.models.source import Source
+from app.utils.workers_utils import save_json
 from app.workers.core.parent_scraper import ParentScraper
 from app.workers.core.query import Query
-from app.workers.core.utils import save_json
 
 
 class GovScraper(ParentScraper):
